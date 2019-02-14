cb7c2a5911fca938589f587cdbeae1240714d00d
AlisherY <alisherelseitov@gmail.com>
2019-02-14 15:43:58 +0600

    comment

---
+++ week1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        
+        private string name;
+        private string id;
+        private int yearofstudy;
+        
+        public Student (string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public string getId()
+        {
+            return this.id;
+        }
+       public int Increment()
+        {
+            return ++yearofstudy;
+        }
+
+       
+    }
+}
